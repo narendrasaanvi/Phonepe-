67f611d40bffa7c4a6317c661a38b6e68edc5a46
Narendra Singh Kushwah <108320570+narendrasaanvi@users.noreply.github.com>
2024-11-07 11:54:36 +0530

    Add files via upload

--- utils/config.php
+++ utils/config.php
@@ -1,1 +1,16 @@
+<?php
 
+define("BASE_URL", "https://sevafoundaton.in/seva-new/ppay/");
+define("API_STATUS", "UAT"); //LIVE OR UAT
+define("MERCHANTIDLIVE", "");
+define("MERCHANTIDUAT", "PGTESTPAYUAT86");  //Sandbox testing
+define("SALTKEYLIVE", " ");
+define("SALTKEYUAT", "96434309-7796-489d-8924-ab56988a6076");
+define("SALTINDEX", "1");
+define("REDIRECTURL", "paymentstatus.php");
+define("SUCCESSURL", "success.php");
+define("FAILUREURL", "failure.php");
+define("UATURLPAY", "https://api-preprod.phonepe.com/apis/pg-sandbox/pg/v1/pay");
+define("LIVEURLPAY", "https://api.phonepe.com/apis/hermes/pg/v1/pay");
+define("STATUSCHECKURL", "https://api-preprod.phonepe.com/apis/pg-sandbox/pg/v1/status/");
+define("LIVESTATUSCHECKURL", "https://api.phonepe.com/apis/hermes/pg/v1/status/");
