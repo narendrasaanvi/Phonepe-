23da9ef315a5ccd2bc4c774404ab1cf513fca586
Narendra Singh Kushwah <108320570+narendrasaanvi@users.noreply.github.com>
2024-12-19 16:38:35 +0530

    Delete clear_session.php

--- clear_session.php
+++
@@ -1,6 +0,0 @@
-<?php
-session_start();
-session_unset(); // Remove all session variables
-session_destroy(); // Destroy the session
-header("Location: ../index.php"); // Redirect to the home page
-exit();
