5ea3a098f34b3c659fbf65a9b7a29fc693d88384
Narendra Singh Kushwah <108320570+narendrasaanvi@users.noreply.github.com>
2024-12-19 16:37:44 +0530

    Add files via upload

--- utils/config.php
+++ utils/config.php
@@ -1,13 +1,13 @@
 <?php
 
-define("BASE_URL", "https://sevafoundaton.in/seva-new/ppay/");
+define("BASE_URL", "https://nakodagems.com/matrimony/pay-now/");
 define("API_STATUS", "UAT"); //LIVE OR UAT
 define("MERCHANTIDLIVE", "");
 define("MERCHANTIDUAT", "PGTESTPAYUAT86");  //For Live Change Value otherwise use Sandbox testing
 define("SALTKEYLIVE", " ");
 define("SALTKEYUAT", "96434309-7796-489d-8924-ab56988a6076"); //For Live Change Value otherwise use Sandbox testing
 define("SALTINDEX", "1");
-define("REDIRECTURL", "paymentstatus.php");
+define("REDIRECTURL", "callback.php");
 define("SUCCESSURL", "success.php");
 define("FAILUREURL", "failure.php");
 define("UATURLPAY", "https://api-preprod.phonepe.com/apis/pg-sandbox/pg/v1/pay");
